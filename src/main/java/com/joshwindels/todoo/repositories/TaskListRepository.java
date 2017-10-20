0dd8b83b14159be680d11a230da44aaacf3468a4
Josh.Windels <joshuawindels@gmail.com>
2017-10-20 11:39:24 +0100

    add task repository skeleton methods

--- src/main/java/com/joshwindels/todoo/repositories/TaskListRepository.java
+++ src/main/java/com/joshwindels/todoo/repositories/TaskListRepository.java
@@ -1,5 +1,9 @@
 package com.joshwindels.todoo.repositories;
 
+import java.util.List;
+
 public interface TaskListRepository {
+
+    List<String> getAllTasks();
 
 }
--- src/main/java/com/joshwindels/todoo/repositories/impl/TaskListRepositoryImpl.java
+++ src/main/java/com/joshwindels/todoo/repositories/impl/TaskListRepositoryImpl.java
@@ -1,7 +1,22 @@
 package com.joshwindels.todoo.repositories.impl;
 
+import java.util.List;
+
 import com.joshwindels.todoo.repositories.TaskListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 public class TaskListRepositoryImpl implements TaskListRepository {
+
+    @Autowired
+    NamedParameterJdbcTemplate npjt;
+
+    @Override public List<String> getAllTasks() {
+
+        String sql = "SELECT * FROM tasks";
+
+        return null;
+
+    }
 
 }
