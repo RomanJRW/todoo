73eabd077b0322860f48d8cb5100ef9bff18b7b0
Josh.Windels <joshuawindels@gmail.com>
2017-10-20 13:40:38 +0100

    remove cookie session stuff from controller, will reimplement persistence in frontend later

--- src/main/java/com/joshwindels/todoo/controllers/TaskListController.java
+++ src/main/java/com/joshwindels/todoo/controllers/TaskListController.java
@@ -1,22 +1,14 @@
 package com.joshwindels.todoo.controllers;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletResponse;
-
 import com.joshwindels.todoo.converters.TaskConverter;
 import com.joshwindels.todoo.dos.Task;
 import com.joshwindels.todoo.dos.TaskList;
 import com.joshwindels.todoo.dtos.TaskDTO;
 import com.joshwindels.todoo.services.CsvConverterService;
 import com.joshwindels.todoo.services.TaskListService;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@Scope("request")
 @RequestMapping("/tasks")
 public class TaskListController {
 
@@ -37,17 +28,8 @@ public class TaskListController {
     @Autowired
     private TaskConverter taskConverter;
 
-    private static final String COOKIE_IDENTIFIER = "toDoCookie";
-
     @GetMapping("/")
-    public String begin(@CookieValue(value = COOKIE_IDENTIFIER, required = false) String cookieValue,
-            HttpServletResponse response) {
-        BeanUtils.copyProperties(taskList, taskListService.getExistingTaskListFromFromFile(cookieValue));
-        if (taskList.getIdentifier() == null) {
-            String identifier = new SimpleDateFormat("yyyyMMddHHmm").format(new Date());
-            response.addCookie(new Cookie(COOKIE_IDENTIFIER, identifier));
-            taskList.setIdentifier(identifier);
-        }
+    public String begin() {
         return "redirect:show";
     }
 
@@ -59,14 +41,14 @@ public class TaskListController {
 
     @PostMapping("/add")
     public String addTask(@RequestBody TaskDTO taskDTO) {
-        Task task =taskConverter.convertToTask(taskDTO);
+        Task task = taskConverter.convertToTask(taskDTO);
         taskListService.addTaskToList(task, taskList);
         return "redirect:show";
     }
 
     @PostMapping("/remove")
     public String removeTask(@RequestBody TaskDTO taskDTO) {
-        Task task =taskConverter.convertToTask(taskDTO);
+        Task task = taskConverter.convertToTask(taskDTO);
         taskListService.removeTaskFromList(task, taskList);
         return "redirect:show";
     }
