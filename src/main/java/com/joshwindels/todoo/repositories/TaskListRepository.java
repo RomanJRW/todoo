6be16b2d0fc09c3cab1c6934fdcc5e73352d9f1c
Josh.Windels <joshuawindels@gmail.com>
2017-10-20 10:51:24 +0100

    add initial task list repo and impl for storage

---
+++ src/main/java/com/joshwindels/todoo/repositories/TaskListRepository.java
@@ -0,0 +1,5 @@
+package com.joshwindels.todoo.repositories;
+
+public interface TaskListRepository {
+
+}
---
+++ src/main/java/com/joshwindels/todoo/repositories/impl/TaskListRepositoryImpl.java
@@ -0,0 +1,7 @@
+package com.joshwindels.todoo.repositories.impl;
+
+import com.joshwindels.todoo.repositories.TaskListRepository;
+
+public class TaskListRepositoryImpl implements TaskListRepository {
+
+}
