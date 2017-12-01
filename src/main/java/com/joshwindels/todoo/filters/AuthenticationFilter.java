42626d933eeae156fce9482e7e6b574a79d0b188
Josh.Windels <joshuawindels@gmail.com>
2017-12-01 09:20:17 +0000

    add user authentication filter and redirect

---
+++ src/main/java/com/joshwindels/todoo/filters/AuthenticationFilter.java
@@ -0,0 +1,46 @@
+package com.joshwindels.todoo.filters;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.joshwindels.todoo.dos.CurrentUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthenticationFilter implements Filter {
+
+    @Autowired
+    CurrentUser currentUser;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        boolean isLoginPage = ((HttpServletRequest) request).getRequestURL().toString().contains("login");
+        if ((!isLoginPage) && (currentUser.getId() == null)) {
+            HttpServletResponse httpResponse = (HttpServletResponse) response;
+            httpResponse.sendRedirect("/todoo/login");
+        }
+        else {
+            chain.doFilter(request, response);
+        }
+        return;
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+}
