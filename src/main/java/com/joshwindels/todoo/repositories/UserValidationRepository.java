8abc0b2cb38f0d1cd7bae95ec5c9ab66a6d3190d
Josh.Windels <joshuawindels@gmail.com>
2017-12-18 23:21:46 +0000

    add repository login validation method and fix some existing issues with process

--- src/main/java/com/joshwindels/todoo/repositories/UserValidationRepository.java
+++ src/main/java/com/joshwindels/todoo/repositories/UserValidationRepository.java
@@ -2,6 +2,6 @@ package com.joshwindels.todoo.repositories;
 
 public interface UserValidationRepository {
 
-    int getUserIdForUserNameAndPassword(String username, String password);
+    Integer getUserIdForUserNameAndPassword(String username, String password);
 
 }
--- src/main/java/com/joshwindels/todoo/repositories/impl/UserValidationRepositoryImpl.java
+++ src/main/java/com/joshwindels/todoo/repositories/impl/UserValidationRepositoryImpl.java
@@ -1,16 +1,29 @@
 package com.joshwindels.todoo.repositories.impl;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.joshwindels.todoo.repositories.UserValidationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class UserValidationRepositoryImpl implements UserValidationRepository {
 
+    @Autowired
+    NamedParameterJdbcTemplate npjt;
+
     @Override
-    public int getUserIdForUserNameAndPassword(String username, String password) {
-
-
-        return 0;
+    public Integer getUserIdForUserNameAndPassword(String username, String encryptedPassword) {
+        String sql = "SELECT user_id "
+                + "   FROM user_details "
+                + "   WHERE username = :username "
+                + "   AND password = :password ";
+        Map<String, String> params = new HashMap<>();
+        params.put("username", username);
+        params.put("password", encryptedPassword);
+        return npjt.queryForObject(sql, params, Integer.class);
     }
 
 }
